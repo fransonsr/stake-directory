672cf1238b8232c3924f474897bb3646fffc6571
fransonsr <fransonsr@familysearch.org>
2013-04-19 10:18:54 -0600

    Discovered easy finder methods on repositories; added finders for Unit.

--- stake-directory-domain/src/main/java/org/provoysa12th/directory/domain/repository/UnitRepository.java
+++ stake-directory-domain/src/main/java/org/provoysa12th/directory/domain/repository/UnitRepository.java
@@ -1,7 +1,20 @@
 package org.provoysa12th.directory.domain.repository;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.provoysa12th.directory.domain.Unit;
+import org.provoysa12th.directory.domain.Unit.Type;
 
 public interface UnitRepository extends BaseRepository<Unit> {
 
+	Unit findByUuid(UUID uuid);
+
+	Unit findByUnitNumber(Integer unitNumber);
+
+	List<Unit> findByName(String name);
+
+	List<Unit> findByNameLike(String regex);
+
+	List<Unit> findByType(Type type);
 }
--- stake-directory-domain/src/test/java/org/provoysa12th/directory/domain/repository/UnitRepositoryComponentTest.java
+++ stake-directory-domain/src/test/java/org/provoysa12th/directory/domain/repository/UnitRepositoryComponentTest.java
@@ -3,11 +3,13 @@ package org.provoysa12th.directory.domain.repository;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.provoysa12th.directory.domain.Unit;
+import org.provoysa12th.directory.domain.Unit.Type;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -45,5 +47,68 @@ public class UnitRepositoryComponentTest {
 		assertThat(actual, is(notNullValue()));
 		assertThat(actual.getUuid(), is(equalTo(uuid)));
 		assertThat(actual.getName(), is("Updated Unit"));
+	}
+
+	@Test
+	public void testFindByUuid() throws Exception {
+		Unit unit = new Unit();
+
+		unit = unitRepository.saveEntity(unit);
+		UUID uuid = unit.getUuid();
+
+		Unit actual = unitRepository.findByUuid(uuid);
+		assertThat(actual, is(notNullValue()));
+		assertThat(actual.getUuid(), is(equalTo(uuid)));
+	}
+
+	@Test
+	public void testFindByName() throws Exception {
+		Unit unit = new Unit();
+		unit.setName("Unit for FindByName");
+
+		unit = unitRepository.saveEntity(unit);
+		UUID uuid = unit.getUuid();
+
+		List<Unit> actual = unitRepository.findByName("Unit for FindByName");
+		assertThat(actual, is(notNullValue()));
+
+		Unit actualUnit = actual.get(0);
+		assertThat(actualUnit.getUuid(), is(equalTo(uuid)));
+	}
+
+	@Test
+	public void testFindByNameLike() throws Exception {
+		Unit unit = new Unit();
+		unit.setName("Test Unit for FindByNameLike");
+
+		unit = unitRepository.saveEntity(unit);
+		UUID uuid = unit.getUuid();
+
+		String regex = ".*FindByNameLike";
+		List<Unit> actual = unitRepository.findByNameLike(regex);
+		assertThat(actual, is(notNullValue()));
+
+		Unit actualUnit = actual.get(0);
+		assertThat(actualUnit.getUuid(), is(equalTo(uuid)));
+	}
+
+	@Test
+	public void testFindByType() throws Exception {
+		Unit ward = new Unit();
+		ward.setType(Type.Ward);
+
+		ward = unitRepository.saveEntity(ward);
+
+		Unit stake = new Unit();
+		stake.setType(Type.Stake);
+
+		stake = unitRepository.saveEntity(stake);
+		UUID uuid = stake.getUuid();
+
+		List<Unit> actual = unitRepository.findByType(Type.Stake);
+		assertThat(actual.size(), is(1));
+
+		Unit actualUnit = actual.get(0);
+		assertThat(actualUnit.getUuid(), is(equalTo(uuid)));
 	}
 }
