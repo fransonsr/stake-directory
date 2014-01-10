9aa67d707410981a3f2199d325f99562bf8919d3
fransonsr <fransonsr@familysearch.org>
2014-01-10 14:34:13 -0700

    Remove EchoResource.

--- server/src/main/java/org/provoysa12th/directory/DirectoryApplication.java
+++ server/src/main/java/org/provoysa12th/directory/DirectoryApplication.java
@@ -3,12 +3,9 @@ package org.provoysa12th.directory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.provoysa12th.directory.rest.impl.WellknownResourcesImpl;
 
-import example.EchoResource;
-
 public class DirectoryApplication extends ResourceConfig {
 
 	public DirectoryApplication() {
-		register(EchoResource.class);
 		register(WellknownResourcesImpl.class);
 	}
 }
