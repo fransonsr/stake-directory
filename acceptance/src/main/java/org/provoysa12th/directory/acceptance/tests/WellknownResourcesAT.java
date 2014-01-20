452ddb7eb2ad0c0eb3468bcc147793b2b5f0b294
fransonsr <fransonsr@familysearch.org>
2014-01-19 18:55:57 -0700

    Refactor to extract WellknownResourcesAT.

--- acceptance/src/main/java/org/provoysa12th/directory/acceptance/tests/WellknownResourcesAT.java
+++ acceptance/src/main/java/org/provoysa12th/directory/acceptance/tests/WellknownResourcesAT.java
@@ -4,25 +4,23 @@ import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
 import javax.ws.rs.client.Client;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.provoysa12th.directory.acceptance.AcceptanceTest;
+import org.provoysa12th.directory.util.XMLResource;
+import org.provoysa12th.directory.util.XMLResourceBuilder;
 import org.provoysa12th.directory.util.XMLResourceBuilderFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.w3c.dom.Element;
 
-@AcceptanceTest("healthcheck")
+@AcceptanceTest("resource")
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {BaseAcceptanceTestConfiguration.class})
-public class HealthCheckAT {
-
-	private static final Logger LOG = LoggerFactory.getLogger(HealthCheckAT.class);
+public class WellknownResourcesAT {
 
 	@Autowired
 	XMLResourceBuilderFactory xmlResourceBuilderFactory;
@@ -34,15 +32,21 @@ public class HealthCheckAT {
 	String baseURI;
 
 	@Test
-	public void testHeartbeat() {
-		String uri = baseURI + "/.well-known/heartbeat";
+	public void testServiceMeta() {
+		String uri = baseURI + "/.well-known/service-meta";
 
-		LOG.info("URI: " + uri);
+		XMLResourceBuilder xmlResourceBuilder = xmlResourceBuilderFactory.newBuilder(uri);
+		XMLResource serviceDiscovery = xmlResourceBuilder
+			.accept(MediaType.APPLICATION_ATOM_XML_TYPE)
+			.build();
 
-		WebTarget target = jerseyClient.target(uri);
-		Response response = target.request().get();
-
-		assertThat(response.getStatus(), is(equalTo(200)));
+		Element xml = serviceDiscovery.rootElement();
+		assertThat(xml, hasXPath("/feed"));
+		assertThat(xml, hasXPath("/feed/id"));
+		assertThat(xml, hasXPath("/feed/updated"));
+		assertThat(xml, hasXPath("/feed/title"));
+		assertThat(xml, hasXPath("/feed/author"));
 	}
+
 
 }
